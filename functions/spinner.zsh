523773e834fea873c849bde200e8fbbf36c5a59a
ZenStarDev <dev@kotik.sh>
2026-06-21 07:01:51 +0700

    update: 0.6

--- functions/spinner.zsh
+++ functions/spinner.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 # functions/spinner.zsh
 
-kotik::spinner::frames=("${(@)")| / - \"}")
+kotik::spinner::frames=( "|" "/" "-" "\\" )
 
 kotik::spinner::show() {
     local i=${1:-0}
